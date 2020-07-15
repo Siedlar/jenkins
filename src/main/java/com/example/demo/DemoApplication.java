737b854e9ee4d6d2045238ac2735262b8896ff45
Krystian.Siedlarski <ksiedlarski@gmail.com>
2020-07-15 11:47:36 +0200

    beczka

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("essa");
+		dodaj(5,4);
 	}
-
+	public static int dodaj(int a,int b){
+		return a+b;
+	}
 }
---
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoApplicationTest {
+
+
+    @Test
+    void dodaj() {
+    assertEquals(10,DemoApplication.dodaj(5,5));
+    }
+}
